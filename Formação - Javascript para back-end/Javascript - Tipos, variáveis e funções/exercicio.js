7d2c33d1ac38051a401fb18dad1380489600f119
Moscarde <theizera@gmail.com>
2022-06-10 23:38:37 -0300

    exercicio

---
+++ Formação - Javascript para back-end/Javascript - Tipos, variáveis e funções/exercicio.js
@@ -0,0 +1,18 @@
+// {
+// 	var respostaDeTudo = 3.14;
+// 	let idade = 42;
+// 	const pi = 29;
+// 	console.log(respostaDeTudo, idade, pi);
+// }
+// console.log(respostaDeTudo, idade, pi);
+
+let minhaLet;
+console.log(minhaLet); //undefined
+
+// atribuindo um valor com TIPO DE DADO STRING
+minhaLet = "eu sou um texto";
+console.log(minhaLet); // "eu sou um texto"
+
+// reatribuindo, desta vez com dado do TIPO NUMBER
+minhaLet = 100;
+console.log(minhaLet); //100
