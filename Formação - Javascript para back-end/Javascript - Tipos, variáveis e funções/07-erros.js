ca0e8aa86fe75b206a6b9980d8dfbf7192c75206
Moscarde <theizera@gmail.com>
2022-06-13 22:43:32 -0300

    aula 3

---
+++ Formação - Javascript para back-end/Javascript - Tipos, variáveis e funções/07-erros.js
@@ -0,0 +1,3 @@
+const numero; // = 1
+//let minhaVar = 2
+console.log(minhaVar)
---
+++ Formação - Javascript para back-end/Javascript - Tipos, variáveis e funções/09-testes.js
@@ -0,0 +1,6 @@
+for (var x = 0; x < 10000; x ++){
+    console.log(x);
+}
+console.time()
+console.log('xx');
+console.timeEnd()
