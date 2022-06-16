dc69ec73df0b5cefa9673cbdbb4c78019a2233f2
Moscarde <theizera@gmail.com>
2022-06-15 23:52:26 -0300

    concat

---
+++ Formação - Javascript para back-end/Javascript - Arrays/06-juntando-listas.js
@@ -0,0 +1,5 @@
+const sala1 = ['Joao', 'Carlos', 'José']
+const sala2 = ['Silvana', 'Marcia', 'Marta']
+
+const salaUnificada = sala1.concat(sala2) //Combines two or more arrays. This method returns a new array without modifying any existing arrays.
+console.log(salaUnificada);
