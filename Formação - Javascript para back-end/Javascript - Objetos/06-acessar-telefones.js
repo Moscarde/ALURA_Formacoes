ad82b71c03f0da6f3338e9841d9c7c76b88b2812
Moscarde <theizera@gmail.com>
2022-06-20 07:19:27 -0300

    array e function - objetos

---
+++ Formação - Javascript para back-end/Javascript - Objetos/06-acessar-telefones.js
@@ -0,0 +1,8 @@
+const cliente = {
+	nome: "Gabriel",
+	fones: ["21992897171", "21992897070"],
+	email: "gabriel@email.com",
+	idade: 24
+};
+
+cliente.fones.forEach((fone) => console.log(fone));
---
+++ Formação - Javascript para back-end/Javascript - Objetos/07-compondo-objetos.js
@@ -0,0 +1,14 @@
+const cliente = {
+	nome: "Gabriel",
+	fones: ["21992897171", "21992897070"],
+	email: "gabriel@email.com",
+	idade: 24
+};
+
+cliente.dependentes = {
+    nome: "Nick",
+    idade: 2,
+
+}
+
+console.log(cliente);
