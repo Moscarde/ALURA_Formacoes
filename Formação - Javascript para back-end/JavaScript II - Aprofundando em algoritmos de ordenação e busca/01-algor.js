00460ad8f2feb190b9edfc1af678b3f31116ab70
Moscarde <theizera@gmail.com>
2022-07-22 01:17:57 -0300

    aula 01

---
+++ Formação - Javascript para back-end/JavaScript II - Aprofundando em algoritmos de ordenação e busca/01-algor.js
@@ -0,0 +1,39 @@
+const { edGalho, edFolha } = require("./Arquivos base/aula-1/arrays")
+
+
+function juntaListas(lista1, lista2) {
+    let listaFinal = [];
+    let posLista1 = 0
+    let posLista2 = 0
+
+    // if (lista1[posLista1].preco < lista2[posLista2].preco) {
+    //     console.log(true);
+    // } else {
+    //     console.log(false);
+    // }
+    // return
+
+    while (posLista1 < lista1.length && posLista2 < lista2.length) {
+
+        if (lista1[posLista1].preco < lista2[posLista2].preco ) {
+            listaFinal.push(lista1[posLista1]);
+            posLista1++
+        } else {
+            listaFinal.push(lista2[posLista2]);
+            posLista2++
+        }
+    }
+
+    while (posLista1 < lista1.length) {
+        listaFinal.push(lista1[posLista1]);
+        posLista1++
+    }
+    while (posLista2 < lista2.length) {
+        listaFinal.push(lista2[posLista2]);
+        posLista2++
+    }
+    console.log(listaFinal);
+
+}
+
+juntaListas(edFolha, edGalho)
