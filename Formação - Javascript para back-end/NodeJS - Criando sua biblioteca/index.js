f858a262b644cb6d95a772cacae6d791c21964c8
Moscarde <theizera@gmail.com>
2022-08-05 05:38:45 -0300

    leitura e tratamento

---
+++ Formação - Javascript para back-end/NodeJS - Criando sua biblioteca/index.js
@@ -0,0 +1,54 @@
+const chalk = require('chalk')
+const fs = require('fs')
+
+function trataErro(err) {
+    throw new Error(chalk.red(err))
+}
+
+async function pegaArquivoAsyncAwait(caminhoDoArquivo) {
+    try {
+        const texto = await fs.promises.readFile(caminhoDoArquivo, 'utf-8')
+        // console.log(chalk.yellow(texto))
+        console.log(extraiLinks(texto))
+    }
+    catch (err) {
+        trataErro(err)
+    }
+    finally {
+        console.log(chalk.yellow('Aplicação finalizada'))
+    }
+
+}
+
+
+
+function extraiLinks(texto) {
+    const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+    const arrayResultados = [];
+    let temp;
+    while ((temp = regex.exec(texto)) !== null) {
+        arrayResultados.push({ [temp[1]]: temp[2] })
+    }
+    return arrayResultados;
+}
+
+function pegaArquivoThenCatch(caminhoDoArquivo) {
+    fs.promises.readFile(caminhoDoArquivo, 'utf-8')
+        .then((texto) => console.log(texto))
+        .catch((err) => trataErro(err))
+}
+
+function pegaArquivo(caminhoDoArquivo) {
+    fs.readFile(caminhoDoArquivo, 'utf-8', (err, data) => {
+        if (err) {
+            trataErro(err)
+        }
+
+        console.log(chalk.blue(data))
+    })
+}
+
+
+// pegaArquivo('./arquivos/readme.mda')
+// pegaArquivoThenCatch('./arquivos/readme.mda')
+pegaArquivoAsyncAwait('./arquivos/readme.md')
